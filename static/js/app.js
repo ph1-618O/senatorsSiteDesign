4e96d93554d83ba6f58782d359b8395eabfe93b6
Jen S <j3n.s3idl@gmail.com>
2020-11-30 13:14:18 -0500

    table slide working, pie chart next

--- static/js/app.js
+++ static/js/app.js
@@ -56,6 +56,25 @@ Promise.all([
     RstateVotesTot.push(ranking[i].Total_Votes);
     Rcontrib.push(ranking[i].Contributions);
   };
+  console.log(Rparty);
+
+console.log(chairs[0]);
+console.log(chairs[0].Candidate);
+
+
+var arrObj = [chairs];
+
+  for (i = 0; i < arrObj.length; i++) {
+    d3.select('tbody')
+      .selectAll('tr')
+      .data(arrObj[i])
+      .enter()
+      .append('tr')
+      .html(function (d) {
+        return `<td>${d.Candidate}</td><td>${d.State_Abbrv}</td><td>${d.Candidate_Votes}</td><td>${d.Committee}</td><td>${d.Contributions}</td>`;
+      });
+  }
+
 
   // d3.select("tbody")
   // .selectAll("tr")
@@ -67,11 +86,66 @@ Promise.all([
   // });
 
   //Call Bar Graph
-//function (t1_xVals, t1_yVals, t2_xVals, t2_yVals, t1_Name, t2_Name, title, divID)
-  createBar(CfullName, Ccontrib, 'Chair Members', RfullName, Rcontrib,  'R_Members', 'Contributions', 'contribBar');
+  //function (t1_xVals, t1_yVals, t2_xVals, t2_yVals, t1_Name, t2_Name, title, divID)
+  createBar(CfullName, Ccontrib, 'Chair Members', RfullName, Rcontrib, 'R_Members', 'Contributions', 'contribBar');
   //Call Scatter Plot
   createScatter(Ccontrib, Cvotes, 'Chairs', Rcontrib, Rvotes, 'R_Members', 'contribCor');
 
+//Table Scroll Vertical
+  // $(document).ready(function () {
+  //   $('#dtDynamicVerticalScrollExample').DataTable({
+  //   "scrollY": "50vh",
+  //   "scrollCollapse": true,
+  //   });
+  //   $('.dataTables_length').addClass('bs-select');
+  //   });
+
+
+  //const arrMin = arr => Math.max(...arr);
+  //const arrMax = arr => Math.max(...arr);
+  // const arrSum = arr => arr.reduce((a,b) => a + b, 0)
+
+  //   var o = { a: 0 };
+
+  // Object.defineProperties(o, {
+  //     'b': { get: function() { return this.a + 1; } },
+  //     'c': { set: function(x) { this.a = x / 2; } }
+  // });
+  // var sumMinMax = {};
+  // sumMinMax.sum = function(arr){
+  //   return this
+  // }
+  // o.c = 10; // Runs the setter, which assigns 10 / 2 (5) to the 'a' property
+  // console.log(o.b); // Runs the getter, which yields a + 1 or 6
+  // console.log(o);
+
+  // const cVotesMin = Cvotes => Math.max(...Cvotes);
+  // console.log(cVotesMin);
+
+  // function sum(obj) {
+  //   return Object.keys(obj).reduce((sum, key) => sum + parseFloat(obj[key] || 0), 0);
+  // }
+
+  // console.log(`sum chair votes:${sum(cVotes)}`);
+
+
+  // const getSumMinMax = function(arr){
+  //   var arrStats = [];
+  //   const arrMin = arr => Math.max(...arr);
+  //   const arrMax = arr => Math.max(...arr);
+  //   const arrSum = arr => arr.reduce((a,b) => a + b, 0)
+  //   arrStats.push(arrMin, arrMax, arrSum);
+
+  //   return arrStats;
+  // }
+
+  // console.log(`Sum, Min, Max Chair Votes: ${getSumMinMax(Cvotes)}`)
+  // console.log((`Sum, Min, Max Chair Votes: ${getSumMinMax(Rvotes)}`))
+
+
+  //v1 committee - sumTotal
+  // createDonut(values, Ccommittee, '% Total Vote/Chair', 'Chairs', 'pieChairs');
+  // createDonut(values, Rcommittee, '% Total Vote/Chair', 'Ranking', 'pieRanking');
 
 
 
@@ -117,7 +191,7 @@ const createBar = function (t1_xVals, t1_yVals, t1_Name, t2_xVals, t2_yVals, t2_
 
 
 
-const createScatter = function(t1_xVals, t1_yVals, t1_Name, t2_xVals, t2_yVals, t2_Name, divID){
+const createScatter = function (t1_xVals, t1_yVals, t1_Name, t2_xVals, t2_yVals, t2_Name, divID) {
   //Scatter
   var trace1 = {
     x: t1_xVals,
@@ -126,7 +200,7 @@ const createScatter = function(t1_xVals, t1_yVals, t1_Name, t2_xVals, t2_yVals, 
     type: 'scatter',
     name: t1_Name
   };
-  
+
   // var trace2 = {
   //   x: [1335104, 926007, 672941, 588166],
   //   y: [4141799, 9883101, 11873431, 4646940],
@@ -134,7 +208,7 @@ const createScatter = function(t1_xVals, t1_yVals, t1_Name, t2_xVals, t2_yVals, 
   //   type: 'scatter',
   //   name: 'Chair V/$$'
   // };
-  
+
   var trace2 = {
     x: t2_xVals,
     y: t2_yVals,
@@ -142,7 +216,7 @@ const createScatter = function(t1_xVals, t1_yVals, t1_Name, t2_xVals, t2_yVals, 
     type: 'scatter',
     name: t2_Name
   };
-  
+
   // var trace4 = {
   //   x: [192243, 1105119, 6019422, 223675],
   //   y: [4940984, 13705548, 1602233, 4408613],
@@ -150,61 +224,61 @@ const createScatter = function(t1_xVals, t1_yVals, t1_Name, t2_xVals, t2_yVals, 
   //   type: 'scatter',
   //   name: 'Ranking V/$$'
   // };
-  
+
   var data = [trace1, trace2];
-  var layout = {title: 'Contributions($$) to Votes'};
+  var layout = {
+    title: 'Contributions($$) to Votes'
+  };
   //calling scatter plot
   Plotly.newPlot(divID, data, layout);
 };
 
 
+//////
+const createDonut = function (values, labels, title, ctrTxt, divID) {
+  var data = [{
+    values: values,
+    labels: labels,
+    domain: {
+      column: 0
+    },
+    // name: tipName,
+    hoverinfo: 'label+percent+name',
+    hole: .4,
+    type: 'pie'
+  }];
 
-
-
-
-
-
-
-
-///
-//Scatter Chart
-// var trace1 = {
-//   x: [1335104, 926007, 672941, 588166],
-//   y: [4141799, 9883101, 11873431, 4646940],
-//   mode: 'markers',
-//   type: 'scatter',
-//   name: 'Chair V/$$',
-// };
-
-// var trace2 = {
-//   x: [1335104, 926007, 672941, 588166],
-//   y: [4141799, 9883101, 11873431, 4646940],
-//   mode: 'lines',
-//   type: 'scatter',
-//   name: 'Chair V/$$'
-// };
-
-// var trace3 = {
-//   x: [192243, 1105119, 6019422, 223675],
-//   y: [4940984, 13705548, 1602233, 4408613],
-//   mode: 'markers',
-//   type: 'scatter',
-//   name: 'Ranking V/$$'
-// };
-
-// var trace4 = {
-//   x: [192243, 1105119, 6019422, 223675],
-//   y: [4940984, 13705548, 1602233, 4408613],
-//   mode: 'lines',
-//   type: 'scatter',
-//   name: 'Ranking V/$$'
-// };
-
-// var data = [trace1, trace2, trace3, trace4];
-// //calling scatter plot
-// Plotly.newPlot('HighestContributions', data);
-
-/////End scatter plot
+  var layout = {
+    title: title,
+    annotations: [{
+      font: {
+        size: 20
+      },
+      showarrow: false,
+      text: ctrTxt,
+      x: .50,
+      y: .53
+    }],
+    height: 300,
+    width: 300,
+    margin: {
+      "t": 0,
+      "b": 0,
+      "l": 0,
+      "r": 0
+    },
+    showlegend: false,
+    legend: {
+      "orientation": "h"
+    },
+    grid: {
+      rows: 0,
+      columns: 0
+    },
+  };
+  // Calling pie chart
+  Plotly.newPlot(divID, data, layout);
+}
 
 
 //Pie Chart
@@ -228,6 +302,7 @@ var layout = {
     },
     showarrow: false,
     text: 'CO2',
+    //loc of center text
     x: .50,
     y: .53
   }],
@@ -240,10 +315,12 @@ var layout = {
     "r": 0
   },
   showlegend: false,
+  legend: {"orientation": "h"},
   grid: {
+    //makes the donut smaller
     rows: 0,
     columns: 0
   },
 };
 // Calling pie chart
-Plotly.newPlot('PieChart', data, layout);
+Plotly.newPlot('pieChairs', data, layout);
