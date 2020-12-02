c8522868a9891e58deaf36cfd81e881622fae549
Jen S <j3n.s3idl@gmail.com>
2020-12-01 23:52:42 -0500

    fixing layout, starting donut on click, making line regress for scatter

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+//main function
 Promise.all([
   d3.csv('../static/data/consolidated_Chairs.csv'),
   d3.csv('../static/data/consolidated_Ranking.csv'),
@@ -58,87 +59,96 @@ Promise.all([
   };
   console.log(Rparty);
 
-console.log(chairs[0]);
-console.log(chairs[0].Candidate);
+  console.log(chairs[0]);
+  console.log(chairs[0].Candidate);
 
-//Propagating Table Row Data from chairs
-//next is drop down to switch to table row data from ranking
-var arrObj = [chairs];
-
-  for (i = 0; i < arrObj.length; i++) {
-    d3.select('tbody')
-      .selectAll('tr')
-      .data(arrObj[i])
-      .enter()
-      .append('tr')
-      .html(function (d) {
-        return `<td>${d.Candidate}</td><td>${d.State_Abbrv}</td><td>${d.Candidate_Votes}</td><td>${d.Committee}</td><td>${d.Contributions}</td>`;
-      });
+  //Propagating Table Row Data from chairs
+  //next is drop down to switch to table row data from ranking
+  const makeTable = function (data) {
+    var arrObj = [data];
+    for (i = 0; i < arrObj.length; i++) {
+      d3.select('tbody')
+        .selectAll('tr')
+        .data(arrObj[i])
+        .enter()
+        .append('tr')
+        .html(function (d) {
+          return `<td>${d.Candidate}</td><td>${d.State_Abbrv}</td><td>${d.Candidate_Votes}</td><td>${d.Committee}</td><td>${d.Contributions}</td>`;
+        });
+    }
   }
 
+  //Making Charts
+  makeTable(chairs);
   //Call Bar Graph
   //function (t1_xVals, t1_yVals, t2_xVals, t2_yVals, t1_Name, t2_Name, title, divID)
   createBar(CfullName, Ccontrib, 'Chair Members', RfullName, Rcontrib, 'R_Members', 'Contributions', 'contribBar');
   //Call Scatter Plot
   createScatter(Ccontrib, Cvotes, 'Chairs', Rcontrib, Rvotes, 'R_Members', 'contribCor');
 
-//Getting Percentages for Donut Chairs
-console.log(Cvotes);
-var sumC = 0;
-for (var i=0; i<Cvotes.length; i++){
-  sumC += parseInt(Cvotes[i]);
-}
-var Cavg = sumC/Cvotes.length;
-console.log(`Avg: ${Cavg}`);
-console.log(`Sum: ${sumC}`);
+  //Getting Percentages for Donut Chairs
+  console.log(Cvotes);
+  var sumC = 0;
+  for (var i = 0; i < Cvotes.length; i++) {
+    sumC += parseInt(Cvotes[i]);
+  }
+  var Cavg = sumC / Cvotes.length;
+  console.log(`Avg: ${Cavg}`);
+  console.log(`Sum: ${sumC}`);
 
-var votePercentC = [];
-function roundUp(num, places) {
-  places = Math.pow(10, places)
-  return Math.ceil(num * places) / places
-}
+  var votePercentC = [];
 
-for (var i = 0; i<Cvotes.length; i++){
-  var holder = Cvotes[i]/sumC*100;
-  votePercentC.push(roundUp(holder, 0));
-};
-//Getting Percentages for Donut Ranking
-console.log(Rvotes);
-var sumR = 0;
-for (var i=0; i<Rvotes.length; i++){
-  sumR += parseInt(Rvotes[i]);
-}
-var Ravg = sumR/Rvotes.length;
-console.log(`Avg: ${Ravg}`);
-console.log(`Sum: ${sumR}`);
+  function roundUp(num, places) {
+    places = Math.pow(10, places)
+    return Math.ceil(num * places) / places
+  }
 
-var votePercentR = [];
-function roundUp(num, places) {
-  places = Math.pow(10, places)
-  return Math.ceil(num * places) / places
-}
+  for (var i = 0; i < Cvotes.length; i++) {
+    var holder = Cvotes[i] / sumC * 100;
+    votePercentC.push(roundUp(holder, 0));
+  };
+  //Getting Percentages for Donut Ranking
+  console.log(Rvotes);
+  var sumR = 0;
+  for (var i = 0; i < Rvotes.length; i++) {
+    sumR += parseInt(Rvotes[i]);
+  }
+  var Ravg = sumR / Rvotes.length;
+  console.log(`Avg: ${Ravg}`);
+  console.log(`Sum: ${sumR}`);
 
-for (var i = 0; i<Rvotes.length; i++){
-  var holder = Rvotes[i]/sumR*100;
-  votePercentR.push(roundUp(holder, 0));
-};
+  var votePercentR = [];
 
-console.log(`Vote Percents: ${votePercentR}`);
-console.log(`Vote Percents: ${votePercentC}`);
-// console.log(roundUp(192.168, 0));
+  function roundUp(num, places) {
+    places = Math.pow(10, places)
+    return Math.ceil(num * places) / places
+  }
 
-createDonut(votePercentC, CfullName, 'Chair Vote %', 'Chairs', 'pieChairs');
-createDonut(votePercentR, RfullName, 'Ranking Member %', 'R_Members', 'pieRanking');
+  for (var i = 0; i < Rvotes.length; i++) {
+    var holder = Rvotes[i] / sumR * 100;
+    votePercentR.push(roundUp(holder, 0));
+  };
+
+  console.log(`Vote Percents: ${votePercentR}`);
+  console.log(`Vote Percents: ${votePercentC}`);
+  // console.log(roundUp(192.168, 0));
+
+  //Making Donuts
+
+  createDonut(votePercentC, CfullName, 'Chair Vote %', 'Chairs', 'pieChairs');
+  // createDonut(votePercentR, RfullName, 'Ranking Member %', 'R_Members', 'pieRanking');
 
 
+  //alert prints on screen
+  //alert(roundNum(3.163303562250186, 2));
 
-//alert prints on screen
-//alert(roundNum(3.163303562250186, 2));
+
 
 }).catch(function (err) {
   //show error
   console.log(err)
 });
+//end main function
 
 
 
@@ -162,11 +172,41 @@ const createBar = function (t1_xVals, t1_yVals, t1_Name, t2_xVals, t2_yVals, t2_
 
   var layout = {
     title: title,
-    showlegend: true
+    showlegend: true,
+    width: 700,
+    height: 450,
+    yaxis: {
+      title: {
+        text: 'Dollars',
+        tickfont: 10,
+
+      },
+    },
+    xaxis: {
+      title: {
+        text: 'Senators',
+        // standoff: 20,
+      },
+      tickangle: 30,
+      // showdividers: true,
+      // dividercolor: 'grey',
+      // dividerwidth: 1,
+      showticklabels: false,
+      tickfont: {
+        size: 10
+      },
+    },
+    margin: {
+      l: 50,
+      r: 50,
+      b: 100,
+      t: 100,
+      pad: 4
+    }
   };
 
   Plotly.newPlot(divID, data, layout, {
-    displayModeBar: false
+    displayModeBar: true
   });
 }
 
@@ -180,23 +220,25 @@ const createScatter = function (t1_xVals, t1_yVals, t1_Name, t2_xVals, t2_yVals,
   var trace1 = {
     x: t1_xVals,
     y: t1_yVals,
-    mode: 'lines+markers',
+    // mode: 'lines+markers',
+    mode: 'markers',
     type: 'scatter',
     name: t1_Name
   };
 
-  // var trace2 = {
-  //   x: [1335104, 926007, 672941, 588166],
-  //   y: [4141799, 9883101, 11873431, 4646940],
-  //   mode: 'lines',
-  //   type: 'scatter',
-  //   name: 'Chair V/$$'
-  // };
+  var trace3 = {
+    x: [3242360],
+    y: [0],
+    mode: 'lines',
+    type: 'scatter',
+    name: 'Chair V/$$'
+  };
 
   var trace2 = {
     x: t2_xVals,
     y: t2_yVals,
-    mode: 'lines+markers',
+    // mode: 'lines+markers',
+    mode: 'markers',
     type: 'scatter',
     name: t2_Name
   };
@@ -209,9 +251,43 @@ const createScatter = function (t1_xVals, t1_yVals, t1_Name, t2_xVals, t2_yVals,
   //   name: 'Ranking V/$$'
   // };
 
-  var data = [trace1, trace2];
+  var data = [trace1, trace2, trace3];
   var layout = {
-    title: 'Contributions($$) to Votes'
+    title: 'Contributions($$) to Votes',
+    width: 700,
+    height: 450,
+    margin: {
+      l: 50,
+      r: 50,
+      b: 100,
+      t: 100,
+      pad: 4
+    },
+    yaxis: {
+      title: {
+        text: 'Votes',
+        tickfont: 10,
+
+      },
+    },
+    xaxis: {
+      title: {
+        text: 'Dollars',
+        standoff: 20
+      },
+      tickangle: 30,
+      // tickfont: {
+      //   size: 15
+      // },
+    },
+    // xaxis: {
+    //   type: 'log',
+    //   autorange: true
+    // },
+    // yaxis: {
+    //   type: 'log',
+    //   autorange: true
+    // }
   };
   //calling scatter plot
   Plotly.newPlot(divID, data, layout);
@@ -236,7 +312,7 @@ const createDonut = function (values, labels, title, ctrTxt, divID) {
     title: title,
     annotations: [{
       font: {
-        size: 20
+        size: 15
       },
       showarrow: false,
       text: ctrTxt,
@@ -266,46 +342,115 @@ const createDonut = function (values, labels, title, ctrTxt, divID) {
 }
 
 
-//Pie Chart
-// var data = [{
-//   values: [16, 15, 12, 6, 5, 4, 42],
-//   labels: ['US', 'China', 'European Union', 'Russian Federation', 'Brazil', 'India', 'Rest of World'],
-//   domain: {
-//     column: 0
-//   },
-//   name: 'GHG Emissions',
-//   hoverinfo: 'label+percent+name',
-//   hole: .4,
-//   type: 'pie'
-// }];
+////update on click
 
-// var layout = {
-//   title: 'Global Emissions 1990-2011',
-//   annotations: [{
-//     font: {
-//       size: 20
-//     },
-//     showarrow: false,
-//     text: 'CO2',
-//     //loc of center text
-//     x: .50,
-//     y: .53
-//   }],
-//   height: 300,
-//   width: 300,
-//   margin: {
-//     "t": 0,
-//     "b": 0,
-//     "l": 0,
-//     "r": 0
-//   },
-//   showlegend: false,
-//   legend: {"orientation": "h"},
-//   grid: {
-//     //makes the donut smaller
-//     rows: 0,
-//     columns: 0
-//   },
-// };
-// // Calling pie chart
-// Plotly.newPlot('pieChairs', data, layout);
+function defaultFunction() {
+  d3.json("data1.json", function (error, newdata) {
+    if (error) throw error;
+    data = newdata;
+    data.forEach(function (d) {
+      d.value = +d.value;
+    })
+    update();
+  });
+}
+
+
+function updateFunction() {
+  d3.json("data2.json", function (error, newdata) {
+    if (error) throw error;
+    data = newdata;
+    update();
+  });
+}
+
+
+
+function update(err, newdata) {
+  y.domain([0, d3.max(data, function (d) {
+    return d.value;
+  })]);
+
+  x.domain(data.map(function (d) {
+      return d.name
+    }))
+    .padding([0.5]);
+}
+
+//////
+
+
+//Lin regress
+
+const linRegress = function (y, x) {
+  var lr = {};
+  var n = y.length;
+  var sumX, sumY, sumXY, sumXX, sumYY;
+  sumX = sumY = sumXY = sumXX = sumYY = 0;
+
+  for (var i = 0; i < y.length; i++) {
+    sumX += x[i];
+    sumY += y[i];
+    sumXY += (x[i] * y[i]);
+    sumXX += (x[i] * x[i]);
+    sumYY += (y[i] * y[i]);
+  }
+  lr['slope'] = (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX);
+  lr['intercept'] = (sumY - lr.slope * sumX) / n;
+  lr['r2'] = Math.pow((n * sumXY - sumX * sumY) / Math.sqrt((n * sumXX - sumX * sumX) * (n * sumYY - sumY * sumY)), 2);
+
+  return lr;
+};
+
+//votes
+var pointsY = [460350,
+  558166,
+  121554,
+  285596,
+  850087,
+  672941,
+  1335104,
+  449017,
+  138149,
+  926007,
+  1378458,
+  4835191,
+  732376,
+  547619,
+  136210,
+  1125999
+];
+
+//contributions
+var pointsX = [8040544,
+  4841285,
+  3772045,
+  2902855,
+  8201067,
+  11056889,
+  4890271,
+  5875081,
+  6058418,
+  9828566,
+  15893492,
+  25254738,
+  6617848,
+  6568817,
+  7380997,
+  18631193
+];
+
+var lr = linRegress(pointsY, pointsX);
+console.log(lr);
+
+// const getBestFit = function(pointsY, pointsX) {
+//   let holder = [];
+//   for (var i = 0; i < pointsY.length; i++){
+//     holder.push()
+
+//   }
+//     return y = mx + b
+// }
+
+
+// for (var i = 0; i < y.length; i++) {
